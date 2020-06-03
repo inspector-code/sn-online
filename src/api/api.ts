074bfcc03cb4f607aeffce2d35b730125cf5b79f
bamboleylo <tsybulko.artem@gmail.com>
2020-06-03 12:12:02 +0300

    Update

---
+++ src/api/api.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import {UserType} from '../types/types';
+
+export const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY": "d664b651-1d4d-4dfc-8e20-6d385698ea30"
+    }
+});
+
+export enum ResultCodesEnum {
+    Success = 0,
+    Error = 1
+}
+
+export enum ResultCodeForCapcthaEnum {
+    CaptchaIsRequired = 10
+}
+
+export type GetItemsType = {
+    items: Array<UserType>
+    totalCount: number
+    error: string | null
+}
+export type APIResponseType<D = {}, RC = ResultCodesEnum> = {
+    data: D
+    messages: Array<string>
+    resultCode: RC
+}
